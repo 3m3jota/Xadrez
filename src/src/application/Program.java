1bc572f3222046aae2c1c77afa9ab0385f1ae67a
3m3jota <juninho.moises98@gmail.com>
2019-06-29 14:18:35 -0300

    Primeira classe: Posicao

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -11,6 +11,7 @@ import src.tabuleiro.Posicao;
 public class Program {
     public static void main(String[] args) {
         Posicao p = new Posicao(3,5);
+        System.out.println(p);
     }
     
 }//fim da classe
