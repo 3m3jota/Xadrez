e7ac5b391e85649c4be34edb096a7c59c5d8c231
3m3jota <juninho.moises98@gmail.com>
2019-07-01 17:45:03 -0300

    Tabuleiro exceção e programação defensiva

--- src/src/tabuleiro/Tabuleiro.java
+++ src/src/tabuleiro/Tabuleiro.java
@@ -7,55 +7,66 @@ package src.tabuleiro;
  *@since Classe criada em 01/07/2019
  */
 public class Tabuleiro {
-private int linha;
-private int coluna;
+private int linhas;
+private int colunas;
 private Peça[][] peças;
 
     public Tabuleiro(int linha, int coluna) {
-        this.linha = linha;
-        this.coluna = coluna;
+        if(linha <1 || coluna <1){
+            throw new TabuleiroExcecao("Erro criando tabuleiro: É necessário que tenha pelo menos 1 linha e 1 coluna");
+        }
+        this.linhas = linha;
+        this.colunas = coluna;
         peças = new Peça[linha][coluna];
     }
 
     public int getLinha() {
-        return linha;
-    }
-
-    public void setLinha(int linha) {
-        this.linha = linha;
+        return linhas;
     }
 
     public int getColuna() {
-        return coluna;
+        return colunas;
     }
 
-    public void setColuna(int coluna) {
-        this.coluna = coluna;
-    }
-   
     public Peça peça(int linha,int coluna){
+        if(!existePosicao(linha, coluna)){
+            throw new TabuleiroExcecao("Não tem essa posição");
+        }
         return peças[linha][coluna];
     }
      
     public Peça peça(Posicao posicao){
+        if(!existePosicao(posicao)){
+            throw new TabuleiroExcecao("Não tem essa posição");
+        }
         return peças[posicao.getLinha()][posicao.getColuna()];
     }
    
     public void posicaoPeça(Peça peça, Posicao posicao){
+        if(temPeça(posicao)){
+            throw new TabuleiroExcecao("Tem uma peça na posição: "+posicao);
+        }
         peças[posicao.getLinha()][posicao.getColuna()] = peça;
         peça.posicao = posicao;
     }
      /*
     public Peça removerPeça(Posicao posicao){
     }
-    
-    public boolean existePeça(Posicao posicao){
-    }
-    
+  
+    */ 
     public boolean temPeça(Posicao posicao){
+        if(!existePosicao(posicao)){
+            throw new TabuleiroExcecao("Não tem essa posição");
+        }
+        return peça(posicao) != null;
     }
     
-    
-*/
+ 
+    private boolean existePosicao(int linha, int coluna){
+        return linha >=0 && linha < linhas && coluna >=0 && coluna < colunas;
+    }
+    public boolean existePosicao(Posicao posicao){
+        return existePosicao(posicao.getLinha(),posicao.getColuna());
+    }
 
 }//fim da classe
---
+++ src/src/tabuleiro/TabuleiroExcecao.java
@@ -0,0 +1,17 @@
+
+package src.tabuleiro;
+
+/**
+ *Classe para...
+ * @author Moisés Júnior
+ *@since Classe criada em 01/07/2019
+ */
+public class TabuleiroExcecao extends RuntimeException{
+
+    public TabuleiroExcecao(String msg) {
+        super(msg);
+    }
+    
+   
+    
+}//fim da classe
